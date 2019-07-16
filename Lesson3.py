e0c93ab01b8bd92d78706a4eafe031c747ddbfab
aryan <aryanmalik567@gmail.com>
2019-07-16 20:53:34 +0100

    First commit

---
+++ Lesson3.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+def test_run():
+    start_date = '2010-01-22'
+    end_date = '2010-01-26'
+    dates = pd.date_range(start_date, end_date)
+
+    #create an empty dataframe
+    df1 = pd.DataFrame(index = dates)
+
+    #create another temporary dataframe
+    dfSPY = pd.read_csv('Data\SPY.csv')
+
+
+if __name__ == "__main__":
+    test_run()
