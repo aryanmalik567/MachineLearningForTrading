aaba3ebde6fb3da5b08f8199e9638edc99c6eec6
aryan <aryanmalik567@gmail.com>
2019-07-17 11:10:26 +0100

    Lesson 11 complete

--- Lesson3.py
+++ Lesson3.py
@@ -6,16 +6,31 @@ def test_run():
     end_date = '2010-01-26'
     dates = pd.date_range(start_date, end_date)
 
-    #create an empty dataframe
-    df1 = pd.DataFrame(index = dates)
+    # create an empty data frame
+    df1 = pd.DataFrame(index=dates)
 
-    #create another temporary dataframe
-    dfSPY = pd.read_csv('Data\SPY.csv')
+    # create another temporary data frame
+    dfSPY = pd.read_csv('Data\SPY.csv', index_col="Date", parse_dates=True,
+                        usecols=['Date', 'Adj Close'], na_values=['nan'])
+
+    # Rename 'Adj Close' column to SPY to prevent clash with other stocks
+    dfSPY = dfSPY.rename(columns={'Adj Close': 'SPY'})
+
+    # Join the two data frames
+    df1 = df1.join(dfSPY, how='inner')
+
+    # Generalised method to add more stocks
+    symbols = ['GOOG', 'IBM', 'GLD']
+    for symbol in symbols:
+        df_temp = pd.read_csv('data/{}.csv'.format(symbol), index_col="Date", parse_dates=True,
+                              usecols=['Date', 'Adj Close'], na_values=['nan'])
+        df_temp = df_temp.rename(columns={'Adj Close': symbol})
+        df1 = df1.join(df_temp)
+
+    print(df1)
 
 
 if __name__ == "__main__":
     test_run()
 
 
-def newFunction():
-    pass
